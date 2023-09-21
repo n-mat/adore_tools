4c9f4664a4f48675f7f0f87cf54f5723447d92f2
Matthias <ngit@mailbox.org>
2023-09-21 22:04:04 +0200

    Update build_all.sh

--- tools/build_all.sh
+++ tools/build_all.sh
@@ -9,15 +9,23 @@ SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export SUBMODULES_PATH="$(pwd)"
 
 
-EXCLUDED_MODULES="lizard_docker make_gadgets apt_cacher_ng_docker catkin_docker ci_teststand cppcheck_docker cpplint_docker catkin_docker"
+EXCLUDED_MODULES="lizard_docker make_gadgets apt_cacher_ng_docker catkin_docker ci_teststand cppcheck_docker cpplint_docker catkin_docker extended_sumo_if_ros"
 EXCLUDED_MODULES=$(echo "$EXCLUDED_MODULES" | tr ' ' '\n' )
 
 
 
 all_modules=$(find . -mindepth 2 -maxdepth 2 -type f -name "*.mk" -exec dirname {} \; | sed "s|./||g")
 
+echo "Building: adore_ml"
+(cd "${SUBMODULES_PATH}/adore_ml" && make build)
+
+
 modules=$(comm -3 <(echo "$all_modules" | sort) <(echo "$EXCLUDED_MODULES" | sort) | sort -r)
 for module in $modules; do
     echo "Building: ${module}"
     (cd "${SUBMODULES_PATH}/${module}" && make build)
 done
+
+echo "Building: extended_sumo_if_ros"
+(cd "${SUBMODULES_PATH}/extended_sumo_if_ros" && make build)
+
